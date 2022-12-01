fd9f07a1b293d51f5af5f91d4e97552424b7bf10
DKYoungOne <bawsinator@gmail.com>
2022-11-30 20:22:22 -0600

    Added filtering functionality.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
+const userIdSelection = document.getElementById("userIdNumber");
 
 const todoList = document.getElementById('todo-list');
 console.log(todoList);
+
+let todoCompletion = true;
 
 let arrayOfTodos = [
     {
@@ -17,26 +20,59 @@ let arrayOfTodos = [
     "completed": false
 }]
 
+
+    
+
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
     .then( (json) => arrayOfTodos = json)
 }
 
-const logTodos = () => {
-    console.log(arrayOfTodos)
+const logTodos = () => console.log(arrayOfTodos);
+
+
+const populateTodos = (array) => { for (let i = 0, len=array.length; i < len; i++) {addElement(i, array)} }
+
+
+const filterTodos = () => {
+    clearTodos()
+    // I needed to use the less specific == to get this to work, since I realized the value is seen as a string rather than a number and I don't
+    // want to take up space converting it to a num. This got my code working as intended.
+    const filteredTodoList = arrayOfTodos.filter(todo => {
+            if (todo.userId == userIdSelection.value) {
+                return true;
+            } else {return false;}    
+        }
+    )
+    populateTodos(filteredTodoList);
+    console.log(filteredTodoList);
+    
 }
 
-const populateTodos = () => {
-    for (let i = 0, len=arrayOfTodos.length, text = arrayOfTodos[i].title; i < len; i++) {addElement(i)}
+const filterTodoByCompletion = (filter) => {
+    todoCompletion = filter;
+    clearTodos()
+    const filteredTodoList = arrayOfTodos.filter(todo => {
+            if (todo.userId == userIdSelection.value && todo.completed === todoCompletion) {
+                return true;
+            } else {return false;}    
+        }
+    )
+    populateTodos(filteredTodoList);
+    console.log(filteredTodoList);
 }
 
-
-const addElement = (i) =>
+const addElement = (i, array) =>
  {
     const newLi = document.createElement('li');
-    const liContent = document.createTextNode(arrayOfTodos[i].title);
+    const liContent = document.createTextNode(array[i].title);
 
     newLi.appendChild(liContent);
     todoList.append(newLi);
  }
+
+ const clearTodos = () =>  todoList.innerHTML = '';
+
+
+ const testUserInput = () => console.log(userIdSelection.value);
